ddccd8daa318bc60bb77eb3000deab0eaf5f41eb
Simanga Hendry <simangahk@gmail.com>
2019-08-23 15:00:32 +0200

    overview page complete

--- index.js
+++ index.js
@@ -26,21 +26,60 @@ fs.readFile('txt/start.txt', 'utf-8', (err, data) => {
 })
 console.log('It will read this')
 */
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+  output = output.replace(/{%IMAGE%}/g, product.image)
+  output = output.replace(/{%PRICE%}/g, product.price)
+  output = output.replace(/{%FROM%}/g, product.from)
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+  output = output.replace(/{%QUANTITY%}/g, product.quantity)
+  output = output.replace(/{%DESCRIPTION%}/g, product.description)
+  output = output.replace(/{%ID%}/g, product.id)
+  // output = output.replace(/{%ID%}/g, product.id)
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not_organic')
+  }
+  return output
+}
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+)
+const tempProducts = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+)
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+)
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
+
 const server = http.createServer((req, res) => {
   // console.log(req)
   let pathName = req.url
 
   if (pathName === '/' || pathName === '/overview') {
-    res.end('Hello from the overview')
+    // OVERVIEW
+    res.writeHead(200, { 'content-type': 'text/html' })
+
+    const cardHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
+    const output = tempOverview.replace('{%PRODUCTS_CARDS%}', cardHtml)
+    console.log(output)
+    res.end(output)
+    // crazy right
   } else if (pathName === '/products') {
+    // PRODUCTS
     res.end('Hello from the products')
   } else if (pathName === '/API') {
+    // PRODUCTS
     res.writeHead(200, { 'content-type': 'application/json' })
     res.end(data)
   } else {
+    // PAGE NOT FOUND
     res.writeHead(404, {
       'content-type': 'text/html',
       'My-Own-Header': 'Hello World'
